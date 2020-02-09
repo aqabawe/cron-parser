98ffa20160fb86af5978fc0d639fc7b64b9032c8
Mamoun Saudi <mamoun@plugify.nl>
2020-02-09 17:17:01 +0100

    RuleMatcher tests

---
+++ test/test_rule_matcher.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative './test_helper'
+
+class TestRuleMatcher < Minitest::Test
+
+  def test_correct_literals
+    assert_equal CronParser::RuleMatcher.match('2'), 'literal_parser'
+    assert_equal CronParser::RuleMatcher.match('99'), 'literal_parser'
+  end
+
+  def test_invalid_literals
+    assert_raises RuntimeError do
+      CronParser::RuleMatcher.match('-2')
+    end
+    assert_raises RuntimeError do
+      CronParser::RuleMatcher.match('999')
+    end
+  end
+
+  def test_correct_ranges
+    assert_equal CronParser::RuleMatcher.match('2-99'), 'range_parser'
+    assert_equal CronParser::RuleMatcher.match('22-1'), 'range_parser'
+    assert_equal CronParser::RuleMatcher.match('1-5/1'), 'range_parser'
+  end
+
+  def test_invalid_ranges
+    assert_raises RuntimeError do
+      CronParser::RuleMatcher.match('2-5-10')
+    end
+    assert_raises RuntimeError do
+      CronParser::RuleMatcher.match('2-5/')
+    end
+    assert_raises RuntimeError do
+      CronParser::RuleMatcher.match('2-/5')
+    end
+    assert_raises RuntimeError do
+      CronParser::RuleMatcher.match('2-999')
+    end
+
+  end
+end
+
